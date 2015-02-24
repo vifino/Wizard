4e87bba0854a7551c91bd911da521a0b1ac865e0
Adrian Pistol <adinator1999@gmail.com>
2015-02-24 22:20:42 +0100

    Create config.exs

---
+++ config/config.exs
@@ -0,0 +1,11 @@
+# This is Wizard's config.
+# 
+# What each arg means is explained now:
+# "Owner": The owner of this bot, change it to your nickname on IRC.
+# "irc.esper.net": The Server
+# 6667: The Port.
+# "WizzyWizard": The bot's nickname.
+# [ "#V" ]: Put your channels here in the following format: [ "#chan1", "#chan2", [ "#chan_with_password", "chanpassword ] ]
+[ wizard:
+  [ conf: [ "Owner", { "irc.esper.net", 6667, "WizzyWizard"}, [ "#V" ] ] ]
+]
