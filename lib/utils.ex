ff8b8f4b46c3824405f2058ea5e1c95f3c04c55a
Adrian Pistol <adinator1999@gmail.com>
2015-03-08 21:17:54 +0100

    Fixed stuffz.

--- lib/utils.ex
+++ lib/utils.ex
@@ -17,21 +17,43 @@ defmodule Utils do
 		:random.uniform(seed)
 	end
 
-	@doc "Runs the string `command` as a shell command and returns the result."
+	@doc "Runs the string `command` as a shell command in /bin/sh and returns the result."
 	def sh(command) when is_bitstring command do
 		if String.strip(command) != "" do
 			to_string(:os.cmd(to_char_list(command)))
+		end
+	end
+	@doc "Runs the string `command` as a shell command in bash and returns the result."
+	def bash(command) when is_bitstring command do
+		if String.strip(command) != "" do
+			elem(System.cmd("bash", ["-c", command], [{:stderr_to_stdout, true}]), 0)
 		end
 	end
 	@doc "Runs the string `command` as a shell command with `pipe_input` piped into it and returns the result."
 	def sh(pipe_input, command) when is_bitstring(pipe_input) and is_bitstring(command) do
 		if String.strip(command) != "" do
 			if pipe_input != "" do
-				#'echo -n #{inspect pipe_input} | #{command}' |> :os.cmd |> to_string
-				to_string(:os.cmd('echo -n #{inspect pipe_input} | #{command}'))
+				IO.puts "echo -n #{quote_str pipe_input} | #{command}"
+				sh("echo -n #{quote_str pipe_input} | #{command}")
 			else
 				sh(command)
 			end
 		end
 	end
+
+	def quote_str(str) do
+		str = str
+			|> replace("\"", "\\\"")
+			|> replace("\\", "\\\\")
+		"\"" <> str <> "\""
+	end
+
+	def replace(source, str, replacement) do
+		regex = if Regex.regex? str do
+			str
+		else
+			Regex.compile!(Regex.escape(str))
+		end
+		Regex.replace(regex, source, replacement)
+	end	
 end
