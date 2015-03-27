928a668703875f6bddf4f7cc422c375bf7f6a42d
Adrian Pistol <adinator1999@gmail.com>
2015-03-27 21:09:55 +0100

    Removed to_llvm because brokeness with llvm 3.6 thingies

--- wizard.sh
+++ wizard.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
-export ERL_COMPILER_OPTIONS="[native, {hipe, [o3, to_llvm]}]"
+export ERL_COMPILER_OPTIONS="[native, {hipe, [o3]}]"
 mix compile || exit 1
 elixir --sname Wizard --erl "+K true" -S mix run -- $@
